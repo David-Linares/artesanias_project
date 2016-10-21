7cb17a023c0d18e12aa1a502b56874bdd38890b6
David Linares <david@e2econtact.solutions>
2016-10-21 09:41:38 -0500

    Cambios en el proyecto funcionando

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by david on 21/10/16.
+ */
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
