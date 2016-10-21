9917524be76c47644ee9473d09741f7db4f49026
David Linares <david@e2econtact.solutions>
2016-10-21 16:44:07 -0500

    Componente para el header

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,11 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: 'app/views/header.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+    variable = "Esta se supone que es la barra de navegación de la página"
+
+}
+
